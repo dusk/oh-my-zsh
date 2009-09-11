4ebc7590ce2b373b567516153da3a32c11832662
alex <aannese@ymr.(none)>
2009-09-10 23:38:53 -0700

    added psql exports

---
+++ export.zsh
@@ -0,0 +1,7 @@
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH:/opt/local/lib/postgresql83/bin/
+export MANPATH=/opt/local/man:$MANPATH
+export DISPLAY=:0.0
+
+export RUBY_HEAP_MIN_SLOTS=800000
+export RUBY_GC_MALLOC_LIMIT=60000000
+export RUBY_HEAP_FREE_MIN=20000
